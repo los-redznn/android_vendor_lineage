eeee53044fd930d2bbf7469f99f37ef2d6779d9c
redznn <h908659@gmail.com>
2024-04-11 21:28:58 -0300

    vendor: Initial fork [1] Props from: https://github.com/crdroidandroid/android_vendor_crdroid/blob/14.0/config/crdroid.mk
    Change-Id: I4a53412c89e4a7ec44156faa46d3538750bc1cbc
    Signed-off-by: redznn <h908659@gmail.com>

---
+++ config/fork.mk
@@ -0,0 +1,28 @@
+# Disable touch video heatmap to reduce latency, motion jitter, and CPU usage
+# on supported devices with Deep Press input classifier HALs and models
+PRODUCT_PRODUCT_PROPERTIES += \
+    ro.input.video_enabled=false
+
+# Workaround AOSP AM crash
+PRODUCT_PROPERTY_OVERRIDES += \
+    sys.fflag.override.settings_enable_monitor_phantom_procs=false
+
+# Blurs
+ifeq ($(TARGET_ENABLE_BLUR), true)
+PRODUCT_SYSTEM_EXT_PROPERTIES += \
+    ro.sf.blurs_are_expensive=1 \
+    ro.surface_flinger.supports_background_blur=1 \
+    ro.launcher.blur.appLaunch=0
+endif
+
+# Disable async MTE on system_server
+PRODUCT_SYSTEM_EXT_PROPERTIES += \
+    arm64.memtag.process.system_server=off
+
+# Enable dex2oat64 to do dexopt
+PRODUCT_SYSTEM_EXT_PROPERTIES += \
+    dalvik.vm.dex2oat64.enabled=true
+
+# Extra packages
+PRODUCT_PACKAGES += \
+    MatLog
